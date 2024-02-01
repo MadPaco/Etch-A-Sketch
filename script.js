0620cd15cc9b6f1f23e6b7fb1cebf67235483fe2
alex <madfoxsec@protonmail.com>
2024-02-01 18:21:40 +0100

    Add basic etch-a-sketch functionality

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let button = document.querySelector('button');
 let size = 16;
+const gridHeight = 600;
+const gridWidth = 600;
+let boxSize = gridHeight/size;
 
 button.addEventListener('click', ()=>{
-    askUserForGridSize();
+    generateGrid(askUserForGridSize())
 });
 
 function askUserForGridSize(){
     size = prompt('Please enter your desired grid size between 1 and 100');
-    if (size > 100 || size < 1){
+    while (size > 100 || size < 1){
         alert('Please enter a number between 1 and 100')
         size = prompt('Please enter your desired grid size between 1 and 100');
+    }
+    boxSize = gridHeight/size;
+    return size;
+}
+
+function generateGrid(size){
+    const gridContainer = document.querySelector('.gridContainer')
+    //calc size for rows and columns in those rows
+    
+    //add rows
+    for (let rowIndex = 0; rowIndex < size; rowIndex++){
+        let row = document.createElement('div');
+        row.style.cssText = `background-color: blue; width: ${gridWidth}px; height: ${boxSize}px;`
+        row.classList.add('row');
+        gridContainer.appendChild(row);
+        //add columns in rows
+        for (let columnIndex = 0; columnIndex < size; columnIndex++){
+            let column = document.createElement('div');
+            column.style.cssText = `background-color: blue; height: ${boxSize}px; width: ${boxSize}px;`
+            column.classList.add('box')
+            column.addEventListener('mouseenter',(event)=>{
+                event.target.style.backgroundColor = 'yellow';
+            });
+            row.appendChild(column);
+        }
     }
 }
